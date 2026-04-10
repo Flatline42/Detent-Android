75201a5d893f3318624d6a8866529f4dbce44910
Flatline42 <72475331+Flatline42@users.noreply.github.com>
2026-04-09 18:45:17 -0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.southsouthwest.framelog"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
